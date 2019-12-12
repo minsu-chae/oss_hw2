4fcdfff893381e8fa74f8927766c74b26dfdc589
minsu-chae <alstn328@naver.com>
2019-12-12 05:04:39 +0000

    commit4

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
 		System.out.println("Changes after commit1!");
+		System.out.println("Make commit4 for bob_project")
 	}
 }
